1ae70ed1ce5831e13bd0a9a47a0ea44abd013e5c
xiangqingxue <77055664@qq.com>
2018-12-03 10:19:50 +0800

    firsrt
    Signed-off-by: xiangqingxue <77055664@qq.com>

---
+++ src/main/java/cn/websocket/receive/ReceiveMessage.java
@@ -0,0 +1,12 @@
+ package cn.websocket.receive;
+
+ public class ReceiveMessage {
+     private String name;
+     
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+     }
+}
